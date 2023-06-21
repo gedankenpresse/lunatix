d80023a0d402891bb5668f9e843d5922fd85a2c4
rnxpyke <rnxpyke@gmail.com>
2023-06-21 20:46:54 +0200

    move printing to module

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -9,19 +9,16 @@ mod device_drivers;
 mod mem;
 mod registers;
 mod userspace;
+mod printk;
 
 use crate::arch::trap::TrapFrame;
 use crate::caps::CSlot;
 use crate::device_drivers::shutdown::{ShutdownCode, SifiveShutdown};
 use crate::device_drivers::uart::Uart;
-use crate::mem::{Page, PAGESIZE};
+use crate::mem::{Page};
 use crate::userspace::fake_userspace;
-use core::fmt;
-use core::fmt::Write;
-use core::mem::size_of;
-use core::ops::{Add, DerefMut};
+use core::ops::{DerefMut};
 use core::panic::PanicInfo;
-use device_drivers::uart::MmUart;
 use fdt_rs::base::DevTree;
 use ksync::SpinLock;
 use thiserror_no_std::private::DisplayAsDisplay;
@@ -40,30 +37,6 @@ fn panic_handler(info: &PanicInfo) -> ! {
     unsafe { shutdown_device.shutdown(ShutdownCode::Fail(1)) }
 }
 
-#[macro_export]
-macro_rules! print {
-    ($($arg:tt)*) => ($crate::_print(format_args!($($arg)*)));
-}
-
-#[macro_export]
-macro_rules! println {
-    () => ($crate::print!("\n"));
-    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
-}
-
-#[doc(hidden)]
-pub fn _print(args: fmt::Arguments) {
-    if let Some(uart) = UART_DEVICE.spin_lock().deref_mut() {
-        uart.write_fmt(args).unwrap();
-    } else {
-        let mut uart = unsafe { Uart::from_ptr(0x1000_0000 as *mut MmUart) };
-        uart.write_str(
-            "Warning: UART device has not been set up. Using hardcoded qemu device pointer.\n",
-        )
-        .unwrap();
-        uart.write_fmt(args).unwrap();
-    }
-}
 
 fn get_memory(dev_tree: &DevTree) -> fdt_rs::error::Result<Option<(u64, u64)>> {
     use fdt_rs::base::DevTree;
@@ -113,18 +86,6 @@ fn init_heap(dev_tree: &DevTree) -> memory::Arena<'static, crate::mem::Page> {
     mem
 }
 
-fn init_caps(mem: memory::Arena<'static, crate::mem::Page>) -> Result<(), caps::Error> {
-    let mut init_memcap = {
-        let content = caps::Memory { inner: mem };
-        caps::Cap::from_content(content)
-    };
-
-    let mut init_cspace = caps::CSpace::init_sz(&mut init_memcap, 8)?;
-    let slot = init_cspace.get_slot_mut(0)?;
-    slot.set(caps::Memory::init_sz(&mut init_memcap, 10)?)?;
-    Ok(())
-}
-
 /// Calculate the stack pointer from a given memory region that should be used as program stack
 unsafe fn calc_stack_start(ptr: *mut Page, num_pages: usize) -> *mut u8 {
     ptr.add(num_pages).cast()
@@ -135,6 +96,12 @@ unsafe fn yield_to(trap_handler_stack: *mut u8, trap_frame: &mut TrapFrame) -> !
     trap_frame.trap_stack = trap_handler_stack.cast();
     arch::trap::trap_frame_restore(trap_frame as *mut TrapFrame, trap_frame.ctx.epc);
 }
+
+unsafe fn set_return_to_user() {
+    let spp: usize = 1 << 8;
+    core::arch::asm!("csrc sstatus, a0",in("a0") spp);
+}
+
 
 #[no_mangle]
 extern "C" fn kernel_main(_hartid: usize, _unused: usize, dtb: *mut u8) {
@@ -185,8 +152,12 @@ extern "C" fn kernel_main(_hartid: usize, _unused: usize, dtb: *mut u8) {
     let userspace_pc = fake_userspace as *const u8 as usize;
     task_state.frame.ctx.epc = userspace_pc;
 
+
     // switch to userspace
-    unsafe { yield_to(trap_handler_stack as *mut u8, &mut task_state.frame) };
+    unsafe { 
+        set_return_to_user();
+        yield_to(trap_handler_stack as *mut u8, &mut task_state.frame)
+    };
 
     // shut down the machine
     let shutdown_device: &mut SifiveShutdown = unsafe { &mut *(0x100_000 as *mut SifiveShutdown) };
---
+++ kernel/src/printk.rs
@@ -0,0 +1,32 @@
+
+use core::fmt;
+use core::fmt::Write;
+use core::ops::DerefMut;
+use crate::UART_DEVICE;
+
+use crate::device_drivers::uart::{MmUart, Uart};
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::printk::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    if let Some(uart) = UART_DEVICE.spin_lock().deref_mut() {
+        uart.write_fmt(args).unwrap();
+    } else {
+        let mut uart = unsafe { Uart::from_ptr(0x1000_0000 as *mut MmUart) };
+        uart.write_str(
+            "Warning: UART device has not been set up. Using hardcoded qemu device pointer.\n",
+        )
+        .unwrap();
+        uart.write_fmt(args).unwrap();
+    }
+}
