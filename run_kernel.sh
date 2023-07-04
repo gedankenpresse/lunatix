e8eb064b730aff650459844259be24fe1c1bc9d5
Finn-Thorben Sell <dev@finn-thorben.me>
2023-07-04 23:38:53 +0200

    change virtualized board to qemu again

--- run_kernel.sh
+++ run_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 qemu-system-riscv64 -s -m 1G \
-    -M sifive_u -bios default \
+    -M virt -bios default \
     -nographic \
     -kernel u-boot.bin \
     -device loader,addr=0x84000000,force-raw=on,file="$1"
